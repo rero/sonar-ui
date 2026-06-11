398e53723b360c70b3cdde9098cb27cecf0a00ff
Pascal Repond <pascal.repond@rero.ch>
2026-06-11 13:47:02 +0200

    chore: release v21.0.0
    * Fixes github actions pinned version to SHAs, remove useless stale
    action, and add dependabot config to update github actions.
    * Updates readme page with up to date badges.
    * Fixes npm run pack to publish correctly to npmjs.
    
    Co-Authored-by: Pascal Repond <pascal.repond@rero.ch>

---
+++ scripts/dist_pkg.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+
+var argv = require('yargs/yargs')(process.argv.slice(2))
+  .usage('Usage: $0 <command> [options]')
+  .example('$0 -o build', 'create a package.json for npmjs')
+  .alias('o', 'outputDir')
+  .nargs('o', 1)
+  .describe('o', 'Output directory')
+  .demandOption(['o'])
+  .help('h')
+  .alias('h', 'help')
+  .epilog('copyright 2019')
+  .argv;
+
+// create package.json file
+let fileName = 'package.json';
+let rawdata = fs.readFileSync(fileName);
+let data = JSON.parse(rawdata);
+delete (data.private);
+delete (data.devDependencies);
+delete (data.dependencies);
+delete (data.scripts);
+fs.writeFileSync(path.join(argv.o, fileName), JSON.stringify(data, null, 2));
